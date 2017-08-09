d0f0f17ede7e0c973a853472a634946f9dce0374
Jiangzhou Liu <jliu212@live.com>
2017-08-09 15:47:07 -0400

    initial commit

---
+++ MvcMovie/Migrations/201707171613418_DataAnnotation2.cs
@@ -0,0 +1,16 @@
+namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotation2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
